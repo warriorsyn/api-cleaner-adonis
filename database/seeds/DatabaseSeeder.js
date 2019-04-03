d691a5fc120ade56bf1ef8698e7fb40fc937ebb3
warriorsyn <mjcgamer32@gmail.com>
2019-04-02 22:45:36 -0300

    full

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -8,31 +8,29 @@ const TimeWorked = use('App/Models/TimeWorked')
 class DatabaseSeeder {
   async run () {
     const user = await User.create({
-      name: 'Joao Victor de Andrade',
-      email: 'joaovictorandrade32@hotmail.com',
-      password: '123456',
-      telephone: '(79) 99154-1139',
-      address: 'Rua 5, Costa nova IV, 500',
+      name: 'Administrator',
+      email: 'admin@yung.com',
+      password: 'admin@yung',
       role: 'administrator'
     })
 
-    const cliente = await User.create({
-      name: 'Cleverson',
-      email: 'cleverson@hotmail.com',
-      password: '123456',
-      telephone: '(79) 99154-1139',
-      address: 'Rua 5, Costa nova IV, 520',
-      role: 'client'
-    })
+    // const cliente = await User.create({
+    //   name: 'Cleverson',
+    //   email: 'cleverson@hotmail.com',
+    //   password: '123456',
+    //   telephone: '(79) 99154-1139',
+    //   address: 'Rua 5, Costa nova IV, 520',
+    //   role: 'client'
+    // })
 
-    const colaborador = await User.create({
-      name: 'Mariana',
-      email: 'mariana@hotmail.com',
-      password: '123456',
-      telephone: '(79) 99154-1139',
-      address: 'Rua 5, Costa nova V, 520',
-      role: 'worker'
-    })
+    // const colaborador = await User.create({
+    //   name: 'Mariana',
+    //   email: 'mariana@hotmail.com',
+    //   password: '123456',
+    //   telephone: '(79) 99154-1139',
+    //   address: 'Rua 5, Costa nova V, 520',
+    //   role: 'worker'
+    // })
 
     const admin = await Role.create({
       slug: 'administrator',
@@ -53,27 +51,27 @@ class DatabaseSeeder {
     await cliente.roles().attach([client.id])
     await user.roles().attach([admin.id])
 
-    const schedule = await user.schedule().create({
-      work: 'Clean the house',
-      date_time: new Date(),
-      status: true,
-      client_id: cliente.id,
-      worker_id: colaborador.id
-    })
+    // const schedule = await user.schedule().create({
+    //   work: 'Clean the house',
+    //   date_time: new Date(),
+    //   status: true,
+    //   client_id: cliente.id,
+    //   worker_id: colaborador.id
+    // })
 
-    await Product.create({
-      name: 'Poison',
-      code: '010928838829374',
-      quantity: 2
-    })
+    // await Product.create({
+    //   name: 'Poison',
+    //   code: '010928838829374',
+    //   quantity: 2
+    // })
 
-    await TimeWorked.create({
-      schedule_id: schedule.id,
-      user_id: colaborador.id,
-      client_id: cliente.id,
-      finished_job: '2019-04-02',
-      time_worked: "4:00"
-    })
+    // await TimeWorked.create({
+    //   schedule_id: schedule.id,
+    //   user_id: colaborador.id,
+    //   client_id: cliente.id,
+    //   finished_job: '2019-04-02',
+    //   time_worked: "4:00"
+    // })
   }
 }
 
